0cd94b13250b26fcb73c47481ea870870c4968d7
John McIntyre <John McIntyre>
2016-11-30 16:12:46 -0800

    camera changes

---
+++ Assets/CameraFade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFade : MonoBehaviour
+{
+    //tutorial
+    //https://www.youtube.com/watch?v=0HwZQt94uHQ
+
+    public Texture2D fadeOutTexture;
+    public float fadeSpeed = 0.8f;
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+
+    void OnGUI()
+    {
+        alpha += fadeDir * fadeSpeed * Time.captureFramerate;
+        alpha = Mathf.Clamp01(alpha);
+
+        //set GUI texture
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+    }
+
+    public float BeginFade(int direction) 
+    {
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    void OnLevelWasLoaded()
+    {
+        BeginFade(-1);
+
+    }
+}
---
+++ Assets/EventHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour {
+
+    public float CameraChange = 2f;
+    private float timer = 0f;
+    private bool fadingFlag = false;
+    public Camera newCam;
+    public Camera currentCam;
+    // Update is called once per frame
+    void Update () {
+        
+        if (timer > CameraChange) {
+            changeCam();
+            print("change");
+            fadingFlag = true;
+        }
+        timer += Time.deltaTime;
+
+        
+	}
+    void changeCam()
+    {
+       // float fadeTime = this.gameObject.GetComponent<CameraFade>().BeginFade(1);
+        //yield return new WaitForSeconds(.8f);
+        newCam.enabled = true;
+        currentCam.enabled = false;
+    }
+}
